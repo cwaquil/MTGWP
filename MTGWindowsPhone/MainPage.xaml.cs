10eba6518198de0e1828e343b04524a948f1b9b9
ronfaraone <ronfaraone@hotmail.com>
2015-04-23 19:03:47 -0300

    Splash Screen

--- MTGWindowsPhone/MainPage.xaml.cs
+++ MTGWindowsPhone/MainPage.xaml.cs
@@ -8,18 +8,67 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using MTGWindowsPhone.Resources;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Controls.Primitives;
+using System.ComponentModel;
+using System.Threading;
 
 namespace MTGWindowsPhone
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private Popup popup;
+        private BackgroundWorker backgroundWorker;
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+            ShowPopup();
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+        }
+
+        private void ShowPopup()
+        {
+            popup = new Popup();
+            popup.Child = new PopupSplash();
+            popup.IsOpen = true;
+
+            StartLoadingData();
+        }
+
+        private void StartLoadingData()
+        {
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += new DoWorkEventHandler(backgroundWorker_DoWork);
+
+            backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker_RunWorkerCompleted);
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(() =>
+            {
+                this.popup.IsOpen = false;
+
+            }
+               );
+        }
+
+        void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            //data Loading
+            Thread.Sleep(5000);
+            
         }
 
         private void btnGreen_Click(object sender, RoutedEventArgs e)
@@ -27,6 +76,7 @@ namespace MTGWindowsPhone
             NavigationService.Navigate(new Uri("/Visualizacoes/Info.xaml?titulo=",
                 UriKind.RelativeOrAbsolute));
         }
+
 
         private void btnBlack_Click(object sender, RoutedEventArgs e)
         {
