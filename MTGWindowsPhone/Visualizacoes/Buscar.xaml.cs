1d8be5b1980f3545f4bea72edc99ad280a5c3ee2
ronfaraone <ronfaraone@hotmail.com>
2015-04-23 16:09:25 -0300

    Filtros de busca

--- MTGWindowsPhone/Visualizacoes/Buscar.xaml.cs
+++ MTGWindowsPhone/Visualizacoes/Buscar.xaml.cs
@@ -11,19 +11,48 @@ using Microsoft.Phone.Shell;
 namespace MTGWindowsPhone.Entidades
 {
     public partial class Buscar : PhoneApplicationPage
-    {
+    {        
+        String[] Forca = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        String[] Resistencia = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+        String[] CMC = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
         public Buscar()
         {
             InitializeComponent();
+            
+            this.lpkForca.ItemsSource = Forca;
+            this.lpkResistencia.ItemsSource = Resistencia;
+            this.lpkCMC.ItemsSource = CMC;
+        }
+               
+        private void chkBranca_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkPreta_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkAzul_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkVerde_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkVermelha_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void btnBuscar_Click(object sender, RoutedEventArgs e)
         {
 
         }
-
-        
-
        
     }
 }
